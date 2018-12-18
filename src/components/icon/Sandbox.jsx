c21944bbd60348cf558ec02792f8fa345c1dda54
Yauhen <silent_control@tut.by>
2018-12-18 10:08:19 +0300

    Minor fix

--- src/components/icon/Sandbox.jsx
+++ src/components/icon/Sandbox.jsx
@@ -29,7 +29,7 @@ const Sandbox = () => (
     <h2><span>3. Icon with size 8rem</span></h2>
     <Icon title="badge" size={8} name="award" />
 
-    <h2><span>1. Disabling icon functionality:</span></h2>
+    <h2><span>4. Disabling icon functionality:</span></h2>
     <Icon size={2} disabled onClick={() => { console.log('!!!'); }} name="atom" />
 
   </Fragment>
